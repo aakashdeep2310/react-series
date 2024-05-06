8b6eb9e6ac781bcb51c3e379367bfe8916fc5b98
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-05-07 00:30:22 +0530

    update

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,24 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+   <h1>Chai aur react || Aakash Deep</h1>
+   <h2>Today i learned about react</h2>
+   <p>Today i learn the basics of react firstly 
+    i learned how to create react project, basically
+     we can create react project through 2 method No. 1
+      is npx create-react-app app-name , then run through npm run
+       start and No. 2 is npm create vite@latest, then enter your project name, 
+       then select the library(react) then select the language(Javascript), hit enter,
+        after creation of project now its time to run the project so write npm run dev in
+         terminal and your project will run</p>
+
+         <Chai/>
+    </>
+
+
+  );
+}
+
+export default App;
