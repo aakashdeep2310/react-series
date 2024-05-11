18f8b4804e816957564ee8cc7821611ce3294fd7
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-05-11 22:25:30 +0530

    update

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {jsx as _jsx} from 'react/jsx-runtime.js'
 import App from './App.jsx'
 
 
@@ -11,9 +12,40 @@ function MyApp(){
   )
 }
 
+// const reactElement = {
+//   type: 'a',
+//   props:{
+//       href : 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+const anotherUser = "Chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target:'_blank' },
+  'click me to visit google',
+  anotherUser,
+  anotherElement
+
+)
+
 ReactDOM.createRoot(document.getElementById('root')).
 render(
+  reactElement
+  
+  // <>
+  //   <MyApp />
+  //   <App/>
+  //   // anotherElement,
+  //   // reactElement,
 
-    <MyApp />
+  //   </>
 
 )
